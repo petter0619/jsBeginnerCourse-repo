d8e13f4e7445919da58d745a40caa939c37b5fdf
Petter Carlsson <pettercarlsson@Petters-iMac.local>
2020-03-30 16:26:01 +0200

    Setup default canvas settings and starting point

--- excercises/etch-a-sketch/etch-a-sketch.js
+++ excercises/etch-a-sketch/etch-a-sketch.js
@@ -1,1 +1,31 @@
 console.log('It works!')
+
+// Select the elements on the page (canves, shake button)
+const canvas = document.querySelector('#etch-a-sketch');
+const ctx = canvas.getContext('2d'); // "The Context" = the place where we do our drawing; aka apply our methods to ("ctx"; common naming convention)
+
+const shakeButton = document.querySelector('.shake');
+
+// Setup our canvas for drawing
+
+const { width, height} = canvas;
+
+ctx.lineJoin = 'round'; //how the lines will look
+ctx.lineCap = 'round'; 
+ctx.lineWidth = 10;
+
+ctx.beginPath(); // The starting point of the drawing
+ctx.moveTo(200, 200); // The starting point location: X pixels in (l>r), Y pixels down.
+ctx.LineTo(200, 200);
+ctx.stroke(); // Above 2 functions make an invisible line, and this "commanc" then strokes it
+
+// Write a draw function
+
+
+// Write a handler for the keys
+
+
+// Write a clear/shake function
+
+
+// Listen for arrow keys
