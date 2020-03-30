ad10070e39b136fe2da0738eac10484033534614
Petter Carlsson <pettercarlsson@Petters-iMac.local>
2020-03-30 16:30:46 +0200

    Randomised starting point for sketching

--- excercises/etch-a-sketch/etch-a-sketch.js
+++ excercises/etch-a-sketch/etch-a-sketch.js
@@ -9,14 +9,16 @@ const shakeButton = document.querySelector('.shake');
 // Setup our canvas for drawing
 
 const { width, height} = canvas;
+let x = Math.floor(Math.random() *  width); // NOTE: The "Math" object must be written with a capital letter
+let y = Math.floor(Math.random() *  height);
 
 ctx.lineJoin = 'round'; //how the lines will look
 ctx.lineCap = 'round'; 
 ctx.lineWidth = 10;
 
 ctx.beginPath(); // The starting point of the drawing
-ctx.moveTo(200, 200); // The starting point location: X pixels in (l>r), Y pixels down.
-ctx.LineTo(200, 200);
+ctx.moveTo(x, y); // The starting point location: X pixels in (l>r), Y pixels down.
+ctx.lineTo(x, y);
 ctx.stroke(); // Above 2 functions make an invisible line, and this "commanc" then strokes it
 
 // Write a draw function
