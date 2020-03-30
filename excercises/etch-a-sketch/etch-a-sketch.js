7dba801462103757ceb7fe2058d6dbacb0f2bf61
Petter Carlsson <pettercarlsson@Petters-iMac.local>
2020-03-30 17:50:45 +0200

    Draw function switch statement

--- excercises/etch-a-sketch/etch-a-sketch.js
+++ excercises/etch-a-sketch/etch-a-sketch.js
@@ -4,6 +4,8 @@ const ctx = canvas.getContext('2d'); // "The Context" = the place where we do ou
 
 const shakeButton = document.querySelector('.shake');
 
+const MOVE_AMOUNT = 10; // Some developers use this naming convection: Caps & underscores, to mark that this is a TRUE constant.
+
 
 // Setup our canvas for drawing
 
@@ -13,7 +15,7 @@ let y = Math.floor(Math.random() *  height);
 
 ctx.lineJoin = 'round'; //how the lines will look
 ctx.lineCap = 'round'; 
-ctx.lineWidth = 10;
+ctx.lineWidth = MOVE_AMOUNT;
 
 ctx.beginPath(); // The starting point of the drawing
 ctx.moveTo(x, y); // The starting point location: X pixels in (l>r), Y pixels down.
@@ -22,17 +24,70 @@ ctx.stroke(); // Above 2 functions make an invisible line, and this "commanc" th
 
 
 // Write a draw function
+function draw({ key }) { // Destructring: Just accepting the "key" key: value pair of the object passed in. You could just have written "options" or "e" to represent the entire object and it would still have worked.
+    ctx.beginPath();
+    ctx.moveTo(x, y);
+
+    switch (key) {
+        case 'ArrowUp':
+            y -= MOVE_AMOUNT;
+            break;
+        case 'ArrowRight':
+            x += MOVE_AMOUNT;
+            break;
+        case 'ArrowDown':
+            y += MOVE_AMOUNT;
+            break;
+        case 'ArrowLeft':
+            x -= MOVE_AMOUNT;
+            break;    
+        default: 
+            break
+    }
+
+    ctx.lineTo(x, y);
+    ctx.stroke();
+
+/*
+    //Start the path
+    ctx.beginPath(); 
+    ctx.moveTo(x, y); //Move the context to where it used to be
+
+    // Move our X and Y values depening on what the user did
+    x = x - 10; //change X value to where we want the point to go
+    y = y - 10; 
+    ctx.lineTo(x, y); //Created a line to the X and Y values (a line between the old and new point)
+    ctx.stroke(); //Drew the line
+*/
+}
+
+/* Options object alternative: 
+
 function draw(options) {
     console.log(options.key);
 }
+
+*/
 
 // Write a handler for the keys
 function handleKey(e) {
     if(e.key.includes('Arrow')) {
         e.preventDefault();
-        draw({ key: e.key });
+        draw({ key: e.key }); //Destructuring: sending just the "key" key: value pair of the e (aka WINDOW) object as the argument. Just passing "e" will work as well.
     }
 }
+
+/* Passing entire object as argument to draw() alternative: 
+
+function handleKey(e) {
+    if(e.key.includes('Arrow')) {
+        e.preventDefault();
+        draw(e);
+    }
+}
+
+*/
+
 
 // Write a clear/shake function
 
