d333d786660a433aba59c21bfc44e1f734815ffb
Petter Carlsson <pettercarlsson@Petters-iMac.local>
2020-03-31 12:48:51 +0200

    handleCardButtonClick function

--- exercises/click outside modal/click-outside.js
+++ exercises/click outside modal/click-outside.js
@@ -1,7 +1,23 @@
 const cardButtons = document.querySelectorAll('.card button');
 
-function handleCardButtonClick() {
-    console.log('Card button clicked!');
+const modalInner = document.querySelector('.modal-inner');
+const modalOuter = document.querySelector('.modal-outer');
+
+
+function handleCardButtonClick(event) {
+    const button = event.currentTarget;
+    const card = button.closest('.card');
+    // Grab the image course
+    const imgSrc = card.querySelector('img').src;
+    const desc = card.dataset.description;
+    const name = card.querySelector('h2').textContent;
+    // Populate the modal with grabbed info
+    modalInner.innerHTML = `
+        <img height="600" width="600" src="${imgSrc.replace('200', '600')}" alt="${name}">
+        <p>${desc}</p> 
+    `;
+    // Show the modal
+    modalOuter.classList.add('open');
 }
 
 cardButtons.forEach(button => button.addEventListener('click', handleCardButtonClick));
