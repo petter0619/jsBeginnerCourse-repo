813e7105cd07892d49648cd2e83d8349cfa6190e
Petter Carlsson <pettercarlsson@Petters-iMac.local>
2020-04-29 11:59:00 +0200

    Slider exercise code along part 1

--- js-beginner-exercises/slider/index.js
+++ js-beginner-exercises/slider/index.js
@@ -1,26 +1,38 @@
-// Slider works by changing the classes of the slides, which alters the transform(X) property (which keeps the slides off-screen)
+// Slider works by changing the classes of the slides, which alters the translateX() property (which keeps the slides off-screen)
+// Slide current = translateX(0); inside slide box, prev = translateX(-100%); just to the left of slide box (so animation makes it slide in), next = translateX(+100%); just to the right of slide box. All are
+// The biggest challenge is simply keeping track of which slide is the current/prev/next one at any given moment
 
-function Slider (slider) {
-  if ( !(slider instanceof Element) ) {     //<-------------------------------
-    throw new Error('No Slider Found!');
+function Slider (slider) {                  // <-------------------- sliderEl name convention (to show it is an element)
+  if ( !(slider instanceof Element) ) {     // <-------------------- Check is something is in fact an ELEMENT (otherwise any value passed in will pass the checker)
+    throw new Error('No slider passed in!');
   }
-  // create some variables for working iwth the slider
-  let prev;
-  let current;
-  let next;
+  // Slide variables for working with the slider
+  let prevSlide;
+  let currentSlide;
+  let nextSlide;
   // Select needed elements
-  const slides = slider.querySelector('.slides');
+  const slides = slider.querySelector('.slides'); // .slides = the wrapper Div containing all the slides (will use firstChildElement & previousSiblingElement, etc to select the actual slides)
   const nextButton = slider.querySelector('.goToNext');
   const prevButton = slider.querySelector('.goToPrev');
 
   // Functions
 
+  // startSlider() function which populates the slide variables on page load
+  function startSlider() {
+    currentSlide = slider.querySelector('.current') || slides.firstElementChild; // <--------------- NOTE: conditional abuse
+    prevSlide = currentSlide.previousElementSibling || slides.lastElementChild;
+    nextSlide = currentSlide.nextElementSibling || slides.firstElementChild;
+
+    console.log(prevSlide, currentSlide, nextSlide);
+  }
+  startSlider();
+
   // move() function which
   function move(direction) {
-    console.log(direction.currenTarget);
+    console.log(direction);
   }
 
-  // addClasses() function
+  // applyClasses() function
   
   // Event listeners
   nextButton.addEventListener('click', move);
