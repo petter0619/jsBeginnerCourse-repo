439b3b3b604f215c4ddd7686e7a91425686be96a
Petter Carlsson <pettercarlsson@Petters-iMac.local>
2020-04-29 14:43:45 +0200

    Slider exercise code along 2

--- js-beginner-exercises/slider/index.js
+++ js-beginner-exercises/slider/index.js
@@ -6,6 +6,9 @@ function Slider (slider) {                  // <-------------------- sliderEl na
   if ( !(slider instanceof Element) ) {     // <-------------------- Check is something is in fact an ELEMENT (otherwise any value passed in will pass the checker)
     throw new Error('No slider passed in!');
   }
+
+  // ---------------------- Variables ---------------------- 
+
   // Slide variables for working with the slider
   let prevSlide;
   let currentSlide;
@@ -15,29 +18,61 @@ function Slider (slider) {                  // <-------------------- sliderEl na
   const nextButton = slider.querySelector('.goToNext');
   const prevButton = slider.querySelector('.goToPrev');
 
-  // Functions
+  // ---------------------- Functions ---------------------- 
 
   // startSlider() function which populates the slide variables on page load
   function startSlider() {
     currentSlide = slider.querySelector('.current') || slides.firstElementChild; // <--------------- NOTE: conditional abuse
     prevSlide = currentSlide.previousElementSibling || slides.lastElementChild;
     nextSlide = currentSlide.nextElementSibling || slides.firstElementChild;
-
-    console.log(prevSlide, currentSlide, nextSlide);
   }
+
+  // When this slider is created, run the startSlider() function
   startSlider();
-
-  // move() function which
-  function move(direction) {
-    console.log(direction);
-  }
+  applyClasses()
 
   // applyClasses() function
+  function applyClasses() {
+    currentSlide.classList.add('current');
+    prevSlide.classList.add('prev');
+    nextSlide.classList.add('next');
+  }
+
+  // move() function; removes classes and adds them to new slides
+  function move(direction) {
+    // Method 1 - Strip classes off of elements
+    const classesToRemove = ['prev', 'current', 'next'];
+    prevSlide.classList.remove(...classesToRemove);
+    currentSlide.classList.remove(...classesToRemove);
+    nextSlide.classList.remove(...classesToRemove);
+    // Method 2: [prev, current, next].forEach(el => el.classList.remove(...classesToRemove));
+
+    if (direction === 'back') {
+      // Destructuring method for reassigning variables to each other
+        // Make a new array of new values & destructure them over and into the old variables
+        // The first thing in new array will = 1st thing in old array, 2nd in new = 2nd in old, etc
+      [prevSlide, currentSlide, nextSlide] = [
+        prevSlide.previousElementSibling || slides.lastElementChild, 
+        prevSlide, 
+        currentSlide,
+      ];
+      // -----> Would this work if you ONLY reset current? Maybe... if you re-ran startSlider()
+    } else {
+      [prevSlide, currentSlide, nextSlide] = [ // <------------------- 29:04 (arrow image)
+        currentSlide, 
+        nextSlide, 
+        nextSlide.nextElementSibling || slides.firstElementChild,
+      ]; 
+    }
+    applyClasses();
+  }
   
-  // Event listeners
+  // ---------------------- Event Listeners ---------------------- 
   nextButton.addEventListener('click', move);
-  prevButton.addEventListener('click', () => move('back')); // The () => needed in order to pass argument 'back' = param 'direction'
+  prevButton.addEventListener('click', () => move('back')); // The () => needed in order pass custom argument 'back' = param 'direction' (within the event listener); OR you can use co pall or apply
 }
 
 const slider1 = Slider(document.querySelector('.slider'));
 const slider2 = Slider(document.querySelector('.dog-slider'));
+
+// Challenge to further develop slider: Add functionality for Arrow Keys on button (if needed) + when someone is focused/tabbed in to the slider DIV itself
