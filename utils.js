5aadf86b51cd2012af9d418ce61f65e1bdf21eb2
Petter Carlsson <pettercarlsson@Petters-iMac.local>
2020-07-29 18:01:41 +0200

    Utility functions JS file

---
+++ utils.js
@@ -0,0 +1,33 @@
+
+// Get a random number between X and Y
+function getRandomBetween(min, max, randomNumber = Math.random()) {
+    return Math.floor(randomNumber * (max - min) + min);
+}
+
+// Return a random item from an array
+function randomItemFromArray(arr, not) {
+    const item = arr[Math.floor(Math.random() * arr.length)];
+    if(item === not) {
+        console.log('----- Used that one last time. Look again. -----');
+        return randomItemFromArray(arr, not);   // Recursion
+    }
+    return item;
+}
+
+// Map over an array sequentially (one after another) and run a callback function
+async function asyncMap(array, callbackFunction) {
+    // Make an array to store our results
+    const results = [];
+    // Loop over our array
+    for(const item of array) {
+        const result = await callbackFunction(item);
+        results.push(result);
+    }
+    // When the loop is done return it
+    return results;
+}
+
+// Wait (place inside an 'async' function with the 'await' keyword in front to wait X ms before running code that follows)
+function wait(ms = 0) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
