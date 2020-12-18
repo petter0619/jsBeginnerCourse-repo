9504b40157e631194db5e459781eb5179586d0a6
Your Name <petter0619@gmail.com>
2020-12-18 14:28:54 +0100

    POST request handled

--- teamTreehouse-fsJS/simpleNodeWebsite/router.js
+++ teamTreehouse-fsJS/simpleNodeWebsite/router.js
@@ -1,21 +1,33 @@
 var Profile = require("./profile.js");
 var renderer = require("./renderer.js");
+var queryString = require('querystring');
+
+var commonHeaders = { 'Content-Type': 'text/html' }; 
 
 // Handle HTTP route GET / and POST / i.e. Home
 function homeRoute(request, response) {
     // if ( url == "/" && GET )
     if( request.url === '/' ) {
-        response.writeHead(200, { 'Content-Type': 'text/plain' });
-        // show search
-        renderer.view('header', {}, response);
-        renderer.view('search', {}, response);
-        renderer.view('footer', {}, response);
-        response.end();
+        if( request.method.toLowerCase() === 'get' ) {
+            response.writeHead(200, commonHeaders); // CommonHeaders = what type of content to serve to the browser e.g: text/html, text/plain, application/json, text/css, text/javascript
+            // show search
+            renderer.view('header', {}, response);
+            renderer.view('search', {}, response);
+            renderer.view('footer', {}, response);
+            response.end();
 
+        // if (url == "/" && POST) redirect to /:username
+        } else if( request.method.toLowerCase() === 'post' ) {
+            // Get the POST data from body
+            request.on('data', function(postBody) {     // Stream event
+                // Extract the username
+                var query = queryString.parse( postBody.toString() );
+                // Redirect to user page
+                response.writeHead(303, { 'Location': '/' + query.username });
+                response.end();
+            });
+        }
     }
-    // if (url == "/" && POST)
-        // redirect to /:username
-    
 }
 
 // Handle HTTP route GET /:username i.e. /chalkers
@@ -23,7 +35,7 @@ function userRoute(request, response) {
     // if ( url == "/...")
     var username = request.url.replace('/', '');
     if(username.length > 0) {
-        response.writeHead(200, { 'Content-Type': 'text/plain' });
+        response.writeHead(200, commonHeaders);
         renderer.view('header', {}, response);
 
         // GET json from Treehouse
