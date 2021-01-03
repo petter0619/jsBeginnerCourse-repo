022ed939e68410662386364e4f224da9c06e4fe7
Your Name <petter0619@gmail.com>
2021-01-03 16:28:32 +0100

    Team Treehouse - Four in a Row project

---
+++ teamTreehouse-fsJS/fourInARow/js/app.js
@@ -0,0 +1,15 @@
+const game = new Game();
+
+document.getElementById('begin-game').addEventListener('click', function() {
+    game.startGame();
+
+    this.style.display = 'none';
+    document.getElementById('play-area').style.opacity = '1';
+});
+
+/**
+ * Listen for keyboard presses
+ */
+document.addEventListener('keydown', function(event) {
+    game.handleKeydown(event);
+});
